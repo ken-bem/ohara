dd641b65a04bd08e4cb02c8410df3ff16f05a175
ken-bem <kenneth.reyes1@upr.edu>
2019-09-15 19:53:33 -0400

    Remove Javascript with security vulnerabilities. Will replace with vue-cli

--- webpack.mix.js
+++
@@ -1,7 +0,0 @@
-let mix = require("laravel-mix");
-
-mix
-    .version()
-    .js("src/main/webapp/**.js", "src/main/resources/static/js")
-    // .sass("", "")
-    .setPublicPath("src/main/resources/static");
